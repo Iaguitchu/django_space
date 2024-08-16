bb8ce08d36e9071a936fb9eae8ff07b20483371d
iago <iago@lakatos>
2024-08-16 16:14:25 -0300

    adicionando classes para o SQLite

--- setup/settings.py
+++ setup/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [ 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
