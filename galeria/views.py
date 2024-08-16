e16b50472a290bd164ca1f7a601f75109dff37a4
iago <iago@lakatos>
2024-08-16 09:33:15 -0300

    criando arquivos partials, e tirando a duplicidade de código

--- galeria/views.py
+++ galeria/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'galeria/index.html')
 # Create your views here.
+
+
+def imagem(request):
+    return render(request,'galeria/imagem.html' )
